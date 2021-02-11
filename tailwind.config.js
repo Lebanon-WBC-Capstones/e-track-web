6db6739bd2f5019ff368e5be8576d550cd908fc6
osamaakb <osama98akb@gmail.com>
2021-02-11 17:33:49 +0300

    Feat: add colors to tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#64CF62',
+        lightGrey: '#F4F4F4',
+      },
+    },
   },
   variants: {
     extend: {},
