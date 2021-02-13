d4817ea56bf4b367432594a758d51c31c279acd7
bassam edelbi <bassamedelbi360@gmail.com>
2021-02-13 13:14:29 +0200

    changes are done

--- src/Components/button/button.js
+++ src/Components/button/button.js
@@ -1,12 +1,14 @@
-import React from "react";
-import './button.css'
-function Button ({ text, onClick }) {
-   
-      return (
-        <button  type="button" onClick={onClick} className="bg-primary hover:bg-green-700 text-white font-bold py-3 px-7 rounded-full " >
-        {text}
-        </button>
-      );
-    
-  }
-  export default Button;
+import React from 'react';
+
+function Button({ text, onClick }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="shadow-md bg-primary hover:bg-gray-100 hover:text-primary text-white font-bold py-3 px-7 rounded-full "
+    >
+      {text}
+    </button>
+  );
+}
+export default Button;
