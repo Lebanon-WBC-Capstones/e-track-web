cf2532b4cb3fbe0377b19393129138a0cdec7543
Abdellatif Edlby <abdellatif_e@hotmail.com>
2021-02-13 19:56:09 +0200

    style: added alt text and removed these symbols " {' '} "

--- src/Pages/HomePage/Header/Header.js
+++ src/Pages/HomePage/Header/Header.js
@@ -6,11 +6,15 @@ import Button from '../../../Components/button/button';
 function Header() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <img src={Logo} className="w-1/12 h-auto absolute left-14 top-9" alt="" />
+      <img
+        src={Logo}
+        className="w-1/12 h-auto absolute left-14 top-9"
+        alt="E-Track Logo"
+      />
       <img
         src={img1}
         className=" absolute top-0 right-0 w-6/12"
-        alt=""
+        alt="background image"
         width="900"
       />
 
@@ -21,11 +25,11 @@ function Header() {
       <div className="absolute top-1/3 right-2/4 h-60">
         <div className="leading-10 font-bold md:text-center ">
           <span className="text-5xl ">
-            Your life is your biggest{' '}
-            <span className="text-primary"> Project </span>{' '}
-          </span>{' '}
+            Your life is your biggest
+            <span className="text-primary"> Project </span>
+          </span>
           <br />
-          <span className="text-7xl">Track it the right way </span>{' '}
+          <span className="text-7xl">Track it the right way </span>
         </div>
 
         <div className="absolute bottom-0 w-full">
