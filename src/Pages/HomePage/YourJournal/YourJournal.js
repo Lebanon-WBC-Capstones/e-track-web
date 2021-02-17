85c024c48a962ec9bc8678bfbb1cef336c0fe3a1
osamaakb <osama98akb@gmail.com>
2021-02-17 23:13:26 +0300

    Fix: added z-index to the button

--- src/Pages/HomePage/YourJournal/YourJournal.js
+++ src/Pages/HomePage/YourJournal/YourJournal.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React from 'react';
 import DataPoints from '../../../assets/images/Data points-pana 1.png';
 import Objects from '../../../assets/images/OBJECTS.png';
-import Button from "../../../Components/button/button.js";
+import Button from '../../../Components/button/button';
 
 export default function YourJournal() {
-    return(
+  return (
     <div>
-        <div className=" top-20 relative ">
-         <p className="text-center text-4xl font-bold font-sans">your journal begins <br/> with a click</p>
-         </div>
-         <div className=" flex justify-center top-20 relative">
-         <Button onClick={()=>{alert("Hello!")}} text={"Get started"} />
-         </div>
-        <img src={Objects} alt="Objects" className="right-0 top-40 absolute "/>
-        <img src={DataPoints} alt="Data" className="relative left-0 top-0 "/>   
+      <div className=" top-10 relative ">
+        <p className="text-center text-4xl font-bold font-sans mb-3">
+          your journal begins <br /> with a click
+        </p>
+      </div>
+      <div className=" flex justify-center top-20 relative z-20">
+        <Button
+          onClick={() => {
+            alert('Hello!');
+          }}
+          text={'Get started'}
+        />
+      </div>
+      <img src={Objects} alt="Objects" className="right-0 top-40 absolute " />
+      <img src={DataPoints} alt="Data" className="relative left-0 top-0 " />
     </div>
-    )}
+  );
+}
