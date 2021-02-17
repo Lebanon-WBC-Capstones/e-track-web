8fc7ca552f6f6a51d7774158bd2aa4b57fe4ed3c
JanaKhanji <janokhanji@gmail.com>
2021-02-17 23:18:48 +0200

    Refactor: change props name

--- src/Components/Calendar/calendar.js
+++ src/Components/Calendar/calendar.js
@@ -1,7 +1,3 @@
-import 'react-calendar/dist/Calendar.css';
-import './calendar.css';
-import Calendar from 'react-calendar';
-
 /*
 the calendar component takes 3 props:
 -classDayfct : a function that return a className to be applied for a specific day based on a condition 
@@ -9,16 +5,21 @@ the calendar component takes 3 props:
     return: class name     //we need to add these classes for calendar.css later
     for   : events and mood style
     
--clicked : a function called when a day is clicked
+-onClick : a function called when a day is clicked
     input :value of the day clicked
     no return 
     for : spreads and moood
    
--value : 
+-startDate : 
   to specify the month that we want to dispplay
-  example: value={new Date(year, month, 1)}
+  example: startDate={new Date(year, month, 1)}
 */
-function CalendarComp({ classDayfct, clicked, value }) {
+
+import 'react-calendar/dist/Calendar.css';
+import './calendar.css';
+import Calendar from 'react-calendar';
+
+function CalendarComp({ classDayfct, onClick, startDate }) {
   return (
     <div className="App">
       <Calendar
@@ -27,9 +28,9 @@ function CalendarComp({ classDayfct, clicked, value }) {
         view="month"
         minDetail="month"
         onClickDay={(value) => {
-          clicked(value);
+          onClick(value);
         }}
-        activeStartDate={value}
+        activeStartDate={startDate}
         tileClassName={({ activeStartDate, date, view }) => classDayfct(date)}
       />
     </div>
