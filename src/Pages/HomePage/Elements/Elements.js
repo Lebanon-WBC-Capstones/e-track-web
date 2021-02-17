ae54fa4bc81afa54b82962088c11764812d6004c
bassam edelbi <bassamedelbi360@gmail.com>
2021-02-18 01:58:19 +0200

    changes requested are done

--- src/Pages/HomePage/Elements/Elements.js
+++ src/Pages/HomePage/Elements/Elements.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import img1 from './images/progress.png';
-import img2 from './images/todo.png';
-import img3 from './images/yoga.png';
-import img4 from './images/reading.png';
-import img5 from './images/calendar.png';
-import img6 from './images/lazy.png';
-import img7 from './images/happy.png';
-import img8 from './images/shape.png';
-import img9 from './images/up.png';
-import img10 from './images/down.png';
+import progress from './images/progress.png';
+import todo from './images/todo.png';
+import yoga from './images/yoga.png';
+import reading from './images/reading.png';
+import calendar from './images/calendar.png';
+import lazy from './images/lazy.png';
+import happy from './images/happy.png';
+import shape from './images/shape.png';
+import up from './images/up.png';
+import down from './images/down.png';
 
 export default function Elements() {
   return (
-    <div>
+    <div className="font-sans font-normal">
       <div
-        style={{ backgroundImage: `url(${img9})` }}
+        style={{ backgroundImage: `url(${up})` }}
         className="bg-no-repeat bg-left-bottom bg-gray-100 h-screen"
       >
-        <div className="">
+        <div>
           <img
-            src={img2}
-            alt=""
+            src={todo}
+            alt={todo}
             className="rounded shadow-md left-32 top-20  absolute "
           />
           <img
-            src={img1}
-            alt=""
+            src={progress}
+            alt={progress}
             className="rounded shadow-md absolute left-96 top-96 w-60 h-50"
           />
 
           <div className=" absolute my-40 mx-30 right-20  bottom-72 top-0 inline-block w-90 ">
-            <p className="leading-10 text-4xl font-sans font-normal">
-              Your Dashboard
-            </p>
-            <p className="text-2xl font-sans font-normal">
+            <p className="leading-10 text-4xl ">Your Dashboard</p>
+            <p className="text-2xl ">
               A place where you can check your To-do list <br />
               and your Goals
             </p>
@@ -42,55 +40,59 @@ export default function Elements() {
         </div>
       </div>
       <div
-        style={{ backgroundImage: `url(${img10})` }}
+        style={{ backgroundImage: `url(${down})` }}
         className="bg-no-repeat bg-left-top bg-gray-100 h-screen"
       >
         <div className="flex ">
           <div className=" mt-80 ml-20 center ">
-            <p className="leading-10 text-4xl font-sans font-normal">
-              Your Trackers
-            </p>
-            <p className="text-2xl font-sans font-normal">
+            <p className="leading-10 text-4xl ">Your Trackers</p>
+            <p className="text-2xl ">
               Here you can keep track on your daily activities and your habits
             </p>
             <br />
           </div>
-          <div className="mt-36">
-            <img src={img3} alt="" className="shadow-md  rounded-3xl m-4 " />
+          <div className="mt-36 flex-col">
             <img
-              src={img4}
-              alt=""
-              className="shadow-md  rounded-3xl m-4 ml-56"
+              src={yoga}
+              alt={yoga}
+              className="shadow-md  rounded-3xl  mr-44 "
+            />
+            <img
+              src={reading}
+              alt={reading}
+              className="shadow-md  rounded-3xl m-4 ml-32"
             />
           </div>
         </div>
       </div>
 
       <div
-        style={{ backgroundImage: `url(${img8})` }}
+        style={{ backgroundImage: `url(${shape})` }}
         className="bg-no-repeat bg-right-top bg-gray-100 h-screen"
       >
         <div className="flex ">
           <div className="absolute z-10">
             <img
-              src={img6}
-              alt=""
+              src={lazy}
+              alt={lazy}
               className="shadow-md  rounded-3xl  mt-64 ml-96"
             />
             <img
-              src={img7}
-              alt=""
+              src={happy}
+              alt={happy}
               className="shadow-md  rounded-3xl mt-2 ml-52 "
             />
           </div>
           <div>
-            <img src={img5} alt="" className="shadow-md  rounded-3xl m-4 " />
+            <img
+              src={calendar}
+              alt={calendar}
+              className="shadow-md  rounded-3xl m-4 "
+            />
           </div>
           <div className=" mt-36 ml-72 ">
-            <p className="leading-10 text-4xl font-sans font-normal">
-              Mood Tracker?
-            </p>
-            <p className="text-2xl font-sans font-normal">
+            <p className="leading-10 text-4xl ">Mood Tracker?</p>
+            <p className="text-2xl ">
               Yes, you can also track your mood during the whole year.
             </p>
             <br />
