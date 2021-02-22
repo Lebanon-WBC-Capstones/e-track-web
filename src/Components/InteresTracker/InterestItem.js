49b8fc22184124a21f5af4fbf91aa952cc5e4637
Nourcova <ibrahamnour22@gmail.com>
2021-02-22 14:24:40 +0200

    Feat: made the changes for habit and interest item

--- src/Components/InteresTracker/InterestItem.js
+++ src/Components/InteresTracker/InterestItem.js
@@ -2,29 +2,47 @@ import React, { useState } from 'react';
 // import Components this is the parent
 
 const InterestItem = ({ title, num, date }) => {
-  const [txtColor, setTxtColor] = useState('text-gray-800');
-  const [borderColor, setBorderColor] = useState('border-gray-800');
-  const [backgroundColor, setBackgroundColor] = useState('bg-white');
+  // const [txtColor, setTxtColor] = useState('text-gray-800');
+  // const [borderColor, setBorderColor] = useState('border-gray-800');
+  // const [backgroundColor, setBackgroundColor] = useState('bg-white');
+
+  const [interest, setInterst] = useState({
+    txtColor: 'text-gray-800',
+    borderColor: 'border-gray-800',
+    backgroundColor: 'bg-white',
+  });
+
   const handleColorChanger = () => {
-    if (backgroundColor === 'bg-white') {
-      setBackgroundColor('bg-primary');
-      setTxtColor('text-white');
-      setBorderColor('border-white');
+    if (interest.backgroundColor === 'bg-white') {
+      setInterst({
+        txtColor: 'text-white',
+        borderColor: 'border-white',
+        backgroundColor: 'bg-primary',
+      });
+
+      // interest.setInterst.backgroundColor('bg-primary');
+      // interest.setInterst.txtColor('text-white');
+      // interest.setInterst.borderColor('border-white');
     } else {
-      setBackgroundColor('bg-white');
-      setTxtColor('text-gray-800');
-      setBorderColor('border-gray-800');
+      setInterst({
+        txtColor: 'text-gray-800',
+        borderColor: 'border-gray-800',
+        backgroundColor: 'bg-white',
+      });
+      // interest.setInterst.backgroundColor('bg-white');
+      // interest.setInterst.txtColor('text-gray-800');
+      // interest.setInterst.borderColor('border-gray-800');
     }
   };
 
   return (
     <div
       onClick={handleColorChanger}
-      className={`${backgroundColor} ${txtColor}   text-lg border-b-2 border-gray-200 flex   justify-between content-center max-w-sm mx-auto border-b-1 box-border h-auto w-auto p-4 items-baseline`}
+      className={`${interest.backgroundColor} ${interest.txtColor}   text-lg border-b-2 border-gray-200 flex   justify-between content-center max-w-sm mx-auto border-b-1 box-border h-auto w-auto p-4 items-baseline`}
     >
       <p className="w-1/3 break-words">{title}</p>
       <p
-        className={`${borderColor} justify-self-auto  p-2  text-sm rounded-full border-2 h-6 w-6 flex items-center justify-center`}
+        className={`${interest.borderColor} justify-self-auto  p-2  text-sm rounded-full border-2 h-6 w-6 flex items-center justify-center`}
       >
         {num}
       </p>
