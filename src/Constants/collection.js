ca293e47bd0b3beb8c5a9a80cf416eb0ece69844
Tommy <tommysytc.tc@gmail.com>
2021-02-23 09:45:42 +0200

    Feat: create the collection card element

---
+++ src/Constants/collection.js
@@ -0,0 +1,19 @@
+const collection = [
+{id:1,
+    name:"Gratitude",
+    color:"#F7C1FF"},
+{id:2,
+    name:"Memories",
+    color:"#C3F2F5"},
+{id:3,
+    name:"Notes",
+    color:"#FFC7C7"},
+{id:4,
+    name:"Goals",
+    color:"#A89AFF"},
+{id:5,
+    name:"Thoughts",
+    color:"#FFE9AF"},
+]
+
+export default collection;
