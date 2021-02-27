e53598547037d4e038f876a17923cf7bfbb3c636
JanaKhanji <janokhanji@gmail.com>
2021-02-27 12:24:25 +0200

    fix: scrolling issue

--- src/Pages/DashboardPage/DashboardPage.js
+++ src/Pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,8 @@
 // Components
 import Events from '../../Components/Dashboard/Events/Events.js';
 import Profile from '../../Components/Dashboard/Profile/Profile.js';
-import Weather from '../../Components/Dashboard/Weather/Weather.js';
 import CalendarComp from '../../Components/Calendar/calendar.js';
 import TodoList from '../../Components/Dashboard/TodoList/Todolist.js';
-import Quote from '../../Components/Dashboard/Quote/Quote.js';
 import Progress from '../../Components/Dashboard/Progress/Progress.js';
 import Mood from '../../Components/Dashboard/Mood/Mood.js';
 import Tracker from '../../Components/Dashboard/Tracker/Tracker.js';
@@ -40,24 +38,16 @@ function DashboardPage() {
             startDate={new Date()}
           />
         </div>
-        <div className="p-2">
-          <Weather
-            location={profile.location.city + ',' + profile.location.country}
-          />
-        </div>
       </div>
       <div className="p-2 w-3/5 sm:w-full sm:order-first md:w-full lg:w-3/5 md:order-first lg:order-none">
         <TodoList />
       </div>
       <div className="sm:w-full md:w-full lg:w-1/5 flex flex-col justify-between">
         <div className="p-2">
-          <Quote />
+          <Mood />
         </div>
         <div className="p-2">
           <Progress goal={goal} />
-        </div>
-        <div className="p-2">
-          <Mood />
         </div>
         <div className="p-2">
           <Tracker tracker={tracker} />
