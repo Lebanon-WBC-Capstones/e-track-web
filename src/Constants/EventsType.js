c1c9a41a0ad920a38d70b065b6ed6218f495c2a3
JanaKhanji <janokhanji@gmail.com>
2021-02-28 12:05:15 +0200

    working on spead page

---
+++ src/Constants/EventsType.js
@@ -0,0 +1,24 @@
+const EventsType = [
+  {
+    id: 0,
+    name: 'Event',
+    gender: 'all',
+  },
+  {
+    id: 1,
+    name: 'Reminder',
+    gender: 'all',
+  },
+  {
+    id: 2,
+    name: 'Birthday',
+    gender: 'all',
+  },
+  {
+    id: 3,
+    name: 'Period',
+    gender: 'female',
+  },
+];
+
+export default EventsType;
--- src/Pages/SpreadPage/SpreadPage.js
+++ src/Pages/SpreadPage/SpreadPage.js
@@ -1,4 +1,25 @@
+import CalendarComp from './../../Components/Calendar/calendar.js';
 function SpreadPage() {
-  return <div>Spread Page</div>;
+  const year = new Date().getFullYear();
+  let calendar = [];
+
+  function test(date) {
+    return null;
+  }
+  for (let i = 0; i < 12; i++) {
+    calendar.push(
+      <CalendarComp
+        key={i}
+        startDate={new Date(year, i, 1)}
+        classDayfct={test}
+        onClick={test}
+      />
+    );
+  }
+  return (
+    <div className="h-5/6 overflow-scroll grid grid-cols-4 flex justify-items-center mx-9 my-7">
+      {calendar}
+    </div>
+  );
 }
 export default SpreadPage;
