bc29a3186c29e20053925443c47b829e433fda7f
Nourcova <ibrahamnour22@gmail.com>
2021-03-02 09:01:50 +0200

    Feat: add onClick and fix the name of the img

--- src/Pages/NoTrack/noTrack.js
+++ src/Pages/NoTrack/noTrack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/images/NoTrack.png';
+import notrack from '../../assets/images/NoTrack.png';
 import Button from '../../Components/button/button.js';
 function noTrack() {
   return (
@@ -7,14 +7,19 @@ function noTrack() {
       <div className="flex flex-col items-center ">
         <div className="flex flex-col items-center relative">
           <img
-            src={Logo}
+            src={notrack}
             className="  bg-center bg-contain bg-white pb-20 px-16 z-0 mt-20 w-10/12 rounded-xl shadow-md"
           />
           <p className="text-xl z-10 absolute bottom-20 -m-4">
             <b> No Trackers Yet!</b>
           </p>
           <div className="text-xs absolute -m-8  bottom-12">
-            <Button text="Add a new Tracker" />
+            <Button
+              text="Add a new Tracker"
+              onClick={() => {
+                alert('Hello!');
+              }}
+            />
           </div>
         </div>
       </div>
