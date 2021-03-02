8d8fb7271d70bb6ea465f5d1792a6ed9309071ab
Tommy <tommysytc.tc@gmail.com>
2021-03-02 17:55:28 +0200

    Refactor: minor change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-=======
 import { useTranslation } from 'react-i18next';
 import LandingPage from './Pages/LandingPage/LandingPage.js';
-import AboutUs from './Pages/AboutPage/OurProject/AboutUs.js';
+import AboutUs from './Pages/AboutPage/AboutUs.js';
 import NotFound from './Pages/NotFound/NotFound.js';
-
+import CollectionCardModal from './Components/CollectionCard/CollectionCardModal.js'
 
 import CollectionPage from './Pages/CollectionPage/CollectionPage.js';
 import InterestPage from './Pages/InterestPage/InterestPage.js';
@@ -18,53 +16,55 @@ import Navbar from './Components/Navbar/Navbar.js';
 
 import 'aos/dist/aos.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
->>>>>>> abfe72a6bbda69b4d4f2b9f7351e384ce67a8f7d
 
 function App() {
   let signedin = true;
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/about" exact>
-            <AboutUs />
-          </Route>
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
-          {signedin ? (
-            <div className="bg-lightGrey h-screen">
-              <Navbar />
-              <Route path="/collections" exact>
-                <CollectionPage />
-              </Route>
-              <Route path="/profile" exact>
-                <ProfilePage />
-              </Route>
-              <Route path="/trackers" exact>
-                <TrackerPage />
-              </Route>
-              <Route path="/Mood" exact>
-                <MoodPage />
-              </Route>
-              <Route path="/spread" exact>
-                <SpreadPage />
-              </Route>
-              <Route path="/interest" exact>
-                <InterestPage />
-              </Route>
-              <Route path="/home" exact>
-                <Dashboard />
-              </Route>
-            </div>
-          ) : (
-            <Route path="*">
-              <NotFound />
-            </Route>
-          )}
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Switch>
+    //       <Route path="/about" exact>
+    //         <AboutUs />
+    //       </Route>
+    //       <Route path="/" exact>
+    //         <LandingPage />
+    //       </Route>
+    //       {signedin ? (
+    //         <div className="bg-lightGrey h-screen">
+    //           <Navbar />
+    //           <Route path="/collections" exact>
+    //             <CollectionPage />
+    //           </Route>
+    //           <Route path="/profile" exact>
+    //             <ProfilePage />
+    //           </Route>
+    //           <Route path="/trackers" exact>
+    //             <TrackerPage />
+    //           </Route>
+    //           <Route path="/Mood" exact>
+    //             <MoodPage />
+    //           </Route>
+    //           <Route path="/spread" exact>
+    //             <SpreadPage />
+    //           </Route>
+    //           <Route path="/interest" exact>
+    //             <InterestPage />
+    //           </Route>
+    //           <Route path="/home" exact>
+    //             <Dashboard />
+    //           </Route>
+    //         </div>
+    //       ) : (
+    //         <Route path="*">
+    //           <NotFound />
+    //         </Route>
+    //       )}
+    //     </Switch>
+    //   </div>
+    // </Router>
+   <CollectionCardModal />
+
+    //}
   );
 }
 
