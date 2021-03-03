3ee30f17e029cf4964f58e806599830294a2ab7d
Nourcova <ibrahamnour22@gmail.com>
2021-03-03 09:32:27 +0200

    another image error in aboutus

--- src/Pages/AboutPage/AboutUs.js
+++ src/Pages/AboutPage/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import About_Us from '../../assets/images/AboutUs.png';
+//import About_Us from '../../assets/images/AboutUs.png';
 import yellow from '../../assets/images/yellow.png';
 import Team from '../../Components/TeamMemberCard/TeamMember';
 import ContactUs from '../../Components/ContactUs/Contact';
@@ -10,7 +10,7 @@ export default function AboutUs() {
     <div className="bg-gray-100 min-h-screen">
       <img src={yellow} alt="yellow" className="absolute right-0 top-0 w-2/3" />
       <div className="flex justify-around h-screen items-center flex-wrap">
-        <img src={About_Us} alt="about us" className="z-10" />
+        {/*<img src={About_Us} alt="about us" className="z-10" />*/}
         <p className=" text-xl text-left font-sans z-10">
           <span className="text-2xl font-medium flex justify-center ">
             Our project
