ad5751962b3c255469993c98a933da0dc32ca6db
bassam edelbi <bassamedelbi360@gmail.com>
2021-03-08 13:57:06 +0200

    design-only

--- src/Pages/InterestPage/InterestPage.js
+++ src/Pages/InterestPage/InterestPage.js
@@ -1,52 +1,133 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Addnew from '../../assets/images/Addnew.png';
+import Search from '../../assets/icons/Search.png';
+import InterestItem from './item.js';
+import TrackerItem from './trackerItem.js';
+import { interest } from '../../data.js';
+// import { searchFilter } from './Filter'
 const InterestPage = () => {
-  const [formData, setFormData] = useState({
-    Goal: '',
-    Dealine: '',
-  });
+  // const [formData, setFormData] = useState({
+  //   Goal: '',
+  //   Dealine: '',
+  // });
 
-  const updateFormData = (event) =>
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
+  // const updateFormData = (event) =>
+  //   setFormData({
+  //     ...formData,
+  //     [event.target.name]: event.target.value,
+  //   });
 
-  const { Goal, Deadline } = formData;
+  // const { Goal, Deadline } = formData;
 
   return (
-    <div className="flex h-5/6 w-11/12 ml-10">
-      <div className="bg-white m-2 w-4/12 h-full border rounded-lg">
-        <div className="flex  h-10 ">
-          <p className=" justify-center ">My Trackers</p>
+    <div className="h-5/6   flex justify-items-center mx-9 my-5">
+      <div className="bg-white shadow m-2 w-4/12 h-full border rounded-lg">
+        <div className="flex justify-center font-bold text-2xl p-6">
+          My Trackers
+        </div>
+        <div className=" shadow-sm w-100 ">
+          <div>
+            {interest.length === 0
+              ? null
+              : interest.map((el) => {
+                  return (
+                    <TrackerItem
+                      item={el.title}
+                      date="12/12/2000"
+                      // key={el.id}
+                      // changeItem={checkTodo}
+                      // deleteItem={deleteTodo}
+                    />
+                  );
+                })}
+          </div>
+          <div className="opacity-30 flex items-center">
+            <form className="w-full flex p-4" autoComplete="off">
+              <img
+                className="h-5 w-5 mr-5 place-self-center cursor-pointer"
+                src={Addnew}
+                alt="add"
+                // onClick={(e) => {
+                //   document.getElementById('input-todo').focus();
+                // }}
+              />
+              <input
+                type="text"
+                id="input-todo"
+                placeholder="Add new Tracker"
+                name="title"
+                className="w-full"
+              />
+            </form>
+          </div>
         </div>
       </div>
 
-      <div className="bg-white m-2 w-8/12 h-full border rounded-lg ">
+      <div className="bg-white shadow m-2 w-8/12 h-full border rounded-lg ">
         <div className="flex justify-center w-full">
-          <input
-            className=" m-3 mr-0 p-2 w-2/5 border rounded 2xl:border-solid border-gray-300 block"
-            value={Goal}
-            onChange={(e) => updateFormData(e)}
-            placeholder="Search"
-            type="search"
-            name="Goal"
-            required
-          />
-          <button type="button">yes</button>
+          <div className="m-3 mr-0 p-2  flex border shadow rounded 2xl:border-solid border-gray-300 block">
+            <input
+              className="  w-4/5 focus-within:border-transparent "
+              // onChange={(e) => updateFormData(e)}
+              placeholder="Search"
+              type="search"
+              name=""
+              required
+            />
+            <img
+              className="h-5 w-5  ml-5 place-self-center cursor-pointer"
+              src={Search}
+              alt="add"
+              // onClick={(e) => {
+              //   document.getElementById('input-todo').focus();
+              // }}
+            />
+          </div>
           <input
             className="  m-3 p-2 w-2/5  border rounded 2xl:border-solid border-gray-300 block"
-            value={Deadline}
-            onChange={(e) => updateFormData(e)}
+            // onChange={(e) => updateFormData(e)}
             placeholder="filter"
             type="text"
             name="Deadline"
             onfocus="(this.type='date')"
-            required
+            selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico"
           />
         </div>
-
-        {/* <input type="text" name="myText" value="Norway"
-       selectBoxOptions="Canada;Denmark;Finland;Germany;Mexico"> */}
+        <div className="flex flex-col items-center">
+          {interest.length === 0
+            ? null
+            : interest.map((el) => {
+                return (
+                  <InterestItem
+                    item={el.title}
+                    key={el.id}
+                    // changeItem={checkTodo}
+                    // deleteItem={deleteTodo}
+                  />
+                );
+              })}
+        </div>
+        <div className="p-3 shadow-sm w-100 ">
+          <div className="opacity-30 flex items-center">
+            <form className="w-full flex" autoComplete="off">
+              <img
+                className="h-5 w-5 mr-5 place-self-center cursor-pointer"
+                src={Addnew}
+                alt="add"
+                // onClick={(e) => {
+                //   document.getElementById('input-todo').focus();
+                // }}
+              />
+              <input
+                type="text"
+                id="input-todo"
+                placeholder="Add new Interest"
+                name="title"
+                className="w-full"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
