e067507fa6aae81c9779bf5ec4b2025ce14f3016
Abdellatif Edlby <abdellatif_e@hotmail.com>
2021-03-08 21:13:46 +0200

    fixed collection page

--- src/Components/CollectionListItem/ListItem.js
+++ src/Components/CollectionListItem/ListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import collection from '../../Constants/collection.js';
 
-function CollectionListItem({ number, type }) {
+function CollectionListItem({ number, type, collectionFilter }) {
   const collectionType = collection.find(
     (collection) => collection.id === type.id
   );
   return (
-    <div className=" flex justify-between border-b-2 w-auto ">
+    <div
+      className=" flex justify-between border-b-2 w-auto "
+      onClick={() => collectionFilter(type.id)}
+    >
       <div className=" m-2 ">{collectionType.name}</div>
       <div
         className={`  m-2 w-6 rounded-full flex justify-center items-center bg-${collectionType.color}`}
