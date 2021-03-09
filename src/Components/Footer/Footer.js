ab3ebf7e474e58ec56af75ef9c070174c23ce601
Tommy <tommysytc.tc@gmail.com>
2021-03-09 13:12:18 +0200

    Refctor: delete the div that contains the follow us section of the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,16 +15,6 @@ function Footer() {
           <img src={logoFooter} className="w-40 h-20 mx-11 p-3" alt="logo" />
         </div>
         <div className="my-7 text-white "> {t(`footer.footerRights`)}</div>
-        <div className="my-7 text-white mx-11 flex space-x-8">
-          <p className="pt-1">{t(`footer.footerFollow`)}</p>
-          <div className="flex flex-row space-x-2 ">
-            <img src={instagram} alt="instagram" width="32px" height="32px" />
-            <img src={dribbble} alt="dribble" width="32px" height="32px" />
-
-            <img src={twitter} alt="twitter" width="32px" height="32px" />
-            <img src={youtube} alt="youtube" width="32px" height="32px" />
-          </div>
-        </div>
       </div>
     </div>
   );
