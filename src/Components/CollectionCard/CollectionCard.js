8a1c53375008bfd075ef29a00e70a28cc90954ad
Abdellatif Edlby <abdellatif_e@hotmail.com>
2021-03-09 21:09:23 +0200

    reduced card shadow

--- src/Components/CollectionCard/CollectionCard.js
+++ src/Components/CollectionCard/CollectionCard.js
@@ -6,7 +6,7 @@ import collection from '../../Constants/collection.js';
 export default function CollectionCard({ card }) {
   const type = collection.find((el) => el.id === card.type.id);
   return (
-    <div className={`bg-${type.color} w-80 m-2 p-2 shadow-2xl rounded-xl`}>
+    <div className={`bg-${type.color} w-80 m-2 p-2 shadow-lg rounded-xl`}>
       <p className="text-3xl text-center font-bold">{card.date}</p>
       <p className="text-1xl text-center font-light">{type.name}</p>
       <p className="text-2xl text-center font-semibold">
