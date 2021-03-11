55ebc4705f6355fab89abe30235001f158f864c3
Tommy <tommysytc.tc@gmail.com>
2021-03-11 09:13:32 +0100

    Refactor: minor fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import { useTranslation } from 'react-i18next';
 import LandingPage from './Pages/LandingPage/LandingPage.js';
 import AboutUs from './Pages/AboutPage/AboutUs.js';
 import NotFound from './Pages/NotFound/NotFound.js';
-<<<<<<< HEAD
-import CollectionCardModal from './Components/CollectionCard/CollectionCardModal.js'
-=======
->>>>>>> 83e1e1a1c0153cbbd4c3a0a84b2bc6e4501776a9
 
 import CollectionPage from './Pages/CollectionPage/CollectionPage.js';
 import InterestPage from './Pages/InterestPage/InterestPage.js';
@@ -23,53 +19,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   let signedin = true;
   return (
-<<<<<<< HEAD
-    // <Router>
-    //   <div>
-    //     <Switch>
-    //       <Route path="/about" exact>
-    //         <AboutUs />
-    //       </Route>
-    //       <Route path="/" exact>
-    //         <LandingPage />
-    //       </Route>
-    //       {signedin ? (
-    //         <div className="bg-lightGrey h-screen">
-    //           <Navbar />
-    //           <Route path="/collections" exact>
-    //             <CollectionPage />
-    //           </Route>
-    //           <Route path="/profile" exact>
-    //             <ProfilePage />
-    //           </Route>
-    //           <Route path="/trackers" exact>
-    //             <TrackerPage />
-    //           </Route>
-    //           <Route path="/Mood" exact>
-    //             <MoodPage />
-    //           </Route>
-    //           <Route path="/spread" exact>
-    //             <SpreadPage />
-    //           </Route>
-    //           <Route path="/interest" exact>
-    //             <InterestPage />
-    //           </Route>
-    //           <Route path="/home" exact>
-    //             <Dashboard />
-    //           </Route>
-    //         </div>
-    //       ) : (
-    //         <Route path="*">
-    //           <NotFound />
-    //         </Route>
-    //       )}
-    //     </Switch>
-    //   </div>
-    // </Router>
-   <CollectionCardModal />
-
-    //}
-=======
     <Router>
       <div>
         <Switch>
@@ -113,7 +62,6 @@ function App() {
         </Switch>
       </div>
     </Router>
->>>>>>> 4e525798c2f5cc99f96c5cfb2c290846f7aef3f8
   );
 }
 
