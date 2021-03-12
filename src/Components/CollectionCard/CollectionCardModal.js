29ed50fe76668151c1ffaa88eca7c2b78845d90d
JanaKhanji <janokhanji@gmail.com>
2021-03-12 10:43:55 +0200

    fix overflow modal card

--- src/Components/CollectionCard/CollectionCardModal.js
+++ src/Components/CollectionCard/CollectionCardModal.js
@@ -37,11 +37,15 @@ export default function CollectionCardModal({ card, setShowModal }) {
     <div>
       <div className="justify-center modal items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-5/12 h-6/12 my-6 mx-auto max-w-3xl modal-overlay">
-          <div className=" rounded-lg h-full shadow-lg relative modal-close flex flex-col w-full text-center bg-white outline-none focus:outline-none">
-            <div className=" items-start justify-between px-20 py-8 h-60 rounded-t modal-content">
-              <h3 className="text-3xl font-semibold">{card.date}</h3>
-              <h5 className="text-1xl font-light my-1">{type.name}</h5>
-              <p className="my-4 text-gray-600 text-lg leading-relaxed font-semibold">
+          <div className=" rounded-lg h-full shadow-lg relative modal-close flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className=" items-start justify-between px-20 py-8 h-60 rounded-t modal-content overflow-y-scroll">
+              <h3 className="text-3xl font-semibold text-center ">
+                {card.date}
+              </h3>
+              <h5 className="text-1xl font-light my-1 text-center ">
+                {type.name}
+              </h5>
+              <p className="my-4 text-gray-600 leading-relaxed font-semibold">
                 {card.text}
               </p>
             </div>
