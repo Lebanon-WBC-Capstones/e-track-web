3c8cfa38bf4d8e2ec99eae4b9bfca991ade1143a
JanaKhanji <janokhanji@gmail.com>
2021-03-13 13:36:50 +0200

    fix tailwind classes issues

---
+++ src/Constants/tailwindClasses.js
@@ -0,0 +1,22 @@
+/**
+   * PurgeCSS: 
+ themes:
+   * text-theme1
+   * text-theme2
+   * text-theme3
+   * text-theme4
+   * text-theme5
+   * bg-theme1
+   * bg-theme2
+   * bg-theme3
+   * bg-theme4
+   * bg-theme5
+ 
+ collection:
+   * bg-pink-300
+   * bg-blue-300
+   * bg-red-300
+   * bg-indigo-400
+   * bg-yellow-200
+
+   */
